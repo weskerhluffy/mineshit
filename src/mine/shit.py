824d291296596b86aebce237d3cb047158b9b179
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2018-02-11 08:27:10 -0600

    mierda recursiva

--- src/mine/shit.py
+++ src/mine/shit.py
@@ -15,20 +15,31 @@ nivel_log = logging.ERROR
 nivel_log = logging.DEBUG
 logger_cagada = None
 
-def mineshit_encuentra_rectangulo(x, y, libres):
-    xr = -1
-    yr = -1
+def mineshit_encuentra_rectangulo(x, y, libres, primera_llamada):
+    resultados = []
+    logger_cagada.debug("x {} y {} libres {}".format(x, y, libres))
+    if not x or not y:
+        return resultados
+    if primera_llamada:
+        min_x = 2
+    else:
+        min_x = 1
     for yi in range(y, 1, -1):
         d = libres // yi
         r = libres % yi
         logger_cagada.debug("probando con {} d {} r {}".format(yi, d, r))
-        if r != 1 and d > 1 and (d < x or (d == x and r <= x)):
-            xr = d
-            yr = yi
-            break
-    assert yr <= y
-    assert xr <= x
-    return xr, yr
+        if min_x <= d <= x:
+            if not r:
+                resultados.append((d, yi))
+                break
+            else:
+                if min_x <= d <= x:
+                    res_tmp = mineshit_encuentra_rectangulo(x - d, yi, r, False)
+                    if res_tmp:
+                        resultados = [(d, yi)] + res_tmp
+                        break
+    logger_cagada.debug("x {} y {} libres {} res {}".format(x, y, libres, resultados))
+    return resultados
 
 def posicion_suma(pos_1, pos_2):
     return [pos_1[0] + pos_2[0], pos_1[1] + pos_2[1]]
@@ -65,61 +76,37 @@ def caca_comun_crea_matrix(x, y, valor):
 def mineshit_core(x, y, m):
     se_volteo = False
     if not m:
-        return caca_comun_crea_matrix(x, y, ".")
+        matrix = caca_comun_crea_matrix(x, y, ".")
+        caca_comun_asigna_valor_en_posicion(matrix, (0, 0), "c")
+        return matrix
     if x > y:
         x, y = y, x
         se_volteo = True
     t = x * y
     libres = t - m
-    xr = -1
-    yr = -1
-    s = -1
     matrix = None
+    rectos = []
     if(x == 1):
         if libres > 1:
-            xr = 1
-            yr = libres
+            rectos = [(1, libres)]
     if(x == 2):
         if libres > 2 and not (libres & 1):
-            xr = 2
-            yr = libres // 2
+            rectos = [(2, libres // 2)]
     if(x > 2):
         if(libres >= 4):
-            xr, yr = mineshit_encuentra_rectangulo(x, y, libres)
+            rectos = mineshit_encuentra_rectangulo(x, y, libres, True)
     
-    if xr != -1:
-        s = libres % yr
+    logger_cagada.debug("para cuadro {}x{} con libres {} se encontron cuadros {}".format(x, y, libres, rectos))
+    if rectos:
         matrix = caca_comun_crea_matrix(x, y, "*")
-    
-    if s != -1 and s:
-        lx = 0
-        ly = 0
         posi = [0, 0]
-        if xr < x:
-            lx = 1
-            ly = s
-            posi[0] = xr
-        else:
-            lx = s
-            ly = 1
-            posi[1] = yr
-        minishit_pinta_recto(matrix, posi, lx, ly)
-    
-#    logger_cagada.debug("la matrix es\n{}".format(caca_comun_imprime_matrix(matrix)))
-    logger_cagada.debug("para cuadro {}x{} con libres {} se encontro cuadro {}x{} i sobran {}".format(x, y, libres, xr, yr, s))
-    if xr != -1:
-        if x == 1:
-            minishit_pinta_recto(matrix, (0, 0), 1, libres)
-        if x == 2:
-            minishit_pinta_recto(matrix, (0, 0), 2, libres // 2)
-        if x > 2:
-            minishit_pinta_recto(matrix, (0, 0), xr, yr)
+        for recto_x, recto_y in rectos:
+            minishit_pinta_recto(matrix, posi, recto_x, recto_y)
+            posi[0] += recto_x
+            logger_cagada.debug("la matrix con recto {}x{} es\n{}".format(recto_x, recto_y, caca_comun_imprime_matrix(matrix)))
         caca_comun_asigna_valor_en_posicion(matrix, (0, 0), "c")
-        logger_cagada.debug("la matrix es\n{}".format(caca_comun_imprime_matrix(matrix)))
         
     if se_volteo:
-        xr, yr = yr, xr
-        x, y = y, x
         if matrix:
             matrix = zip(*matrix[::])
     return matrix
@@ -136,9 +123,11 @@ def mineshit_main():
         mamada = mineshit_core(x, y, m)
         print("Case #{}:".format(i + 1))
         if(mamada):
-            print("{}".format(caca_comun_imprime_matrix(mamada)))
+            pass
+#            print("{}".format(caca_comun_imprime_matrix(mamada)))
         else:
-            print("Impossible")
+            pass
+#            print("Impossible")
 
 if __name__ == '__main__':
         FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
